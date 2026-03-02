9cad4b72a8696f106ba2f3a0ddf04c6cd9d93c7a
Amaresh Maharana <amareshcoder@gmail.com>
2026-03-02 15:34:22 +0530

    refactor: comment out title in Home component for cleaner layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1 className="text-2xl underline">BiblioMate AI</h1>
+        {/*<h1 className="text-2xl underline">BiblioMate AI</h1>*/}
       </div>
     </>
   );
