9b6fc97e1663a80c118ea0e145e5d4f1e383b51e
Amaresh Maharana <amareshcoder@gmail.com>
2026-03-03 21:57:46 +0530

    feat: implement HeroSection and display sample books using BookCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
+import {sampleBooks} from "@/lib/constant";
+import BookCard from "@/components/BookCard";
 
 export default function Home() {
   return (
     <>
-      <div>
-        {/*<h1 className="text-2xl underline">BiblioMate AI</h1>*/}
-      </div>
+      <main className="wrapper container">
+        <HeroSection/>
+
+        <div className="library-books-grid">
+          {sampleBooks.map((book) => (
+            <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug}/>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
