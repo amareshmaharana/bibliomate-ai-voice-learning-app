72281f288b56120824836dce3ddd8e3e7b581dc2
Amaresh Maharana <amareshcoder@gmail.com>
2026-03-03 21:57:20 +0530

    refactor: update Navbar logo text style for improved visibility

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                 <div className="wrapper navbar-height py-4 flex justify-between items-center">
                     <Link href="/" className="flex gap-0.5 items-center">
                         <Image src="/assets/logo.png" alt="logo" width={42} height={26}/>
-                        <span className="logo-text">BiblioMate AI</span>
+                        <span className="font-bold text-xl">BiblioMate AI</span>
                     </Link>
 
                     <nav className="w-fit flex gap-7.5 items-center">
